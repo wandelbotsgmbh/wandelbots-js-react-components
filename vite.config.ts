7b12cd2bbf860abbe4c0511a9c7bba4b35ea79d9
Evаn Summers <evan.summers@wandelbots.com>
2024-08-01 20:20:11 +0200

    fix: Package now built with vite, includes src + declarationMap (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    copyPublicDir: false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: Object.keys(pkg.dependencies).concat(Object.keys(pkg.peerDependencies)),
+    },
+    sourcemap: true
+  },
   plugins: [react(), svgr({
     // svgr options: https://react-svgr.com/docs/options/
     svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
