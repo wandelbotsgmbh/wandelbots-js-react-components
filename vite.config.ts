b1b367b1e7b0a95bc8d6a38c20554d3b14206bb5
Evan Summers <evan.summers@wandelbots.com>
2024-08-01 16:11:10 +0200

    Externalize dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +12,12 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs'],
-      fileName: "index"
+      fileName: "index",
     },
-    sourcemap: true,
-    // Let the downstream project do minification
-    minify: false
+    rollupOptions: {
+      external: Object.keys(pkg.dependencies).concat(Object.keys(pkg.peerDependencies)),
+    },
+    sourcemap: true
   },
   plugins: [react(), svgr({
     // svgr options: https://react-svgr.com/docs/options/
