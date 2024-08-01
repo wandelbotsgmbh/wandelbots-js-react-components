6f8adec95c53efb2877d9211e858f5524b72e179
Evan Summers <evan.summers@wandelbots.com>
2024-08-01 15:58:19 +0200

    Revert "Revert "ci: Use vite for production build""
    This reverts commit 93852b42a6bafd82c41af80652d5c083ca700760.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    copyPublicDir: false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: "index"
+    },
+    sourcemap: true,
+    // Let the downstream project do minification
+    minify: false
+  },
   plugins: [react(), svgr({
     // svgr options: https://react-svgr.com/docs/options/
     svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
