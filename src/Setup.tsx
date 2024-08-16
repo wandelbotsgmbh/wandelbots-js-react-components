64e514ead76774c959d3d66182aec6491e8a65b6
Stefan Wagner <mail.bompo@gmail.com>
2024-08-16 11:04:51 +0200

    doc: add story book component for SupportedRobot (#22)
    Co-authored-by: Stefan Wagner <stefan.wagner@wandelbots.com>

---
+++ src/Setup.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { Vector3 } from "three"
+import { Canvas, type Props as CanvasProps } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { PresetEnvironment } from "./components/3d-viewport/PresetEnvironment"
+
+type Props = React.PropsWithChildren<
+  CanvasProps & {
+    cameraFov?: number
+    cameraPosition?: Vector3
+    controls?: boolean
+    lights?: boolean
+  }
+>
+
+export const Setup = ({
+  children,
+  cameraFov = 25,
+  cameraPosition = new Vector3(-2, 1, 1),
+  controls = true,
+  lights = true,
+  ...restProps
+}: Props) => (
+  <Canvas
+    shadows
+    camera={{ position: cameraPosition, fov: cameraFov }}
+    {...restProps}
+  >
+    {children}
+    {lights && (
+      <>
+        <PresetEnvironment />
+      </>
+    )}
+    {controls && <OrbitControls makeDefault />}
+  </Canvas>
+)
