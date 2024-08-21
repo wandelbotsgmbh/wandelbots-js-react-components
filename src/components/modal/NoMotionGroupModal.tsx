ad6a2f458eb20e62a8b04558b15ddec0a77c74d2
Stefan Wagner <stefan.wagner@wandelbots.com>
2024-08-21 14:23:51 +0200

    fix: pass baseUrl to modal

--- src/components/modal/NoMotionGroupModal.tsx
+++ src/components/modal/NoMotionGroupModal.tsx
@@ -7,8 +7,6 @@ import {
   Stack,
   Typography,
 } from "@mui/material"
-import { observer } from "mobx-react-lite"
-import { externalizeComponent } from "../../externalizeComponent"
 
 function getBaseUrl(): string {
   const currentUrl = window.location.href
@@ -18,77 +16,77 @@ function getBaseUrl(): string {
   )
 }
 
-export const NoMotionGroupModal = externalizeComponent(
-  observer((baseUrl: string = getBaseUrl()) => {
-    return (
-      <Dialog
-        aria-labelledby="no-motion-group-modal-title"
-        open={true}
-        fullWidth
+export type NoMotionGroupModalProps = {
+  baseUrl: string
+}
+
+export function NoMotionGroupModal({
+  baseUrl = getBaseUrl(),
+  ...props
+}: NoMotionGroupModalProps) {
+  return (
+    <Dialog
+      aria-labelledby="no-motion-group-modal-title"
+      open={true}
+      fullWidth
+      sx={{
+        "& .MuiModal-backdrop": {
+          backdropFilter: "blur(10px)",
+        },
+        "& .MuiDialog-paper": {
+          "::before": {
+            content: '""',
+            height: "8px",
+            width: "100%",
+            background: "linear-gradient(90deg, #FF0E65 0%, #47D3FF 100%)",
+          },
+          background: "#101629",
+        },
+      }}
+    >
+      <DialogTitle id="no-motion-group-modal-title">
+        No motion group found
+      </DialogTitle>
+
+      <DialogContent
         sx={{
-          "& .MuiModal-backdrop": {
-            backdropFilter: "blur(10px)",
-          },
-          "& .MuiDialog-paper": {
-            "::before": {
-              content: '""',
-              height: "8px",
-              width: "100%",
-              background: "linear-gradient(90deg, #FF0E65 0%, #47D3FF 100%)",
-            },
-            background: "#101629",
-          },
+          marginTop: "3rem",
+          marginBottom: "1.5rem",
+          textAlign: "center",
         }}
       >
-        <DialogTitle id="no-motion-group-modal-title">
-          No motion group found
-        </DialogTitle>
+        <Typography color="#fff">Found no devices to connect to.</Typography>
+        <Typography color="#fff">
+          Please ensure a motion group has been created.
+        </Typography>
+      </DialogContent>
 
-        <DialogContent
-          sx={{
-            marginTop: "3rem",
-            marginBottom: "1.5rem",
-            textAlign: "center",
-          }}
-        >
-          <Typography color="#fff">Found no devices to connect to.</Typography>
-          <Typography color="#fff">
-            Please ensure a motion group has been created.
-          </Typography>
-        </DialogContent>
-
-        <DialogActions>
-          <Stack
-            width="100%"
-            maxWidth="300px"
-            margin="auto"
-            marginBottom="2rem"
+      <DialogActions>
+        <Stack width="100%" maxWidth="300px" margin="auto" marginBottom="2rem">
+          <Button
+            href={`${baseUrl}/settings`}
+            variant="contained"
+            sx={{
+              borderRadius: "8px",
+              width: "100%",
+            }}
           >
-            <Button
-              href={`${baseUrl}/settings`}
-              variant="contained"
-              sx={{
-                borderRadius: "8px",
-                width: "100%",
-              }}
-            >
-              Go to settings app
-            </Button>
-            <Button
-              href={`${baseUrl}`}
-              variant="text"
-              sx={{
-                marginTop: "1rem",
-                color: "#fff",
-                textDecoration: "underline",
-                fontSize: "0.75rem",
-              }}
-            >
-              Back to Homescreen
-            </Button>
-          </Stack>
-        </DialogActions>
-      </Dialog>
-    )
-  }),
-)
+            Go to settings app
+          </Button>
+          <Button
+            href={`${baseUrl}`}
+            variant="text"
+            sx={{
+              marginTop: "1rem",
+              color: "#fff",
+              textDecoration: "underline",
+              fontSize: "0.75rem",
+            }}
+          >
+            Back to Homescreen
+          </Button>
+        </Stack>
+      </DialogActions>
+    </Dialog>
+  )
+}
