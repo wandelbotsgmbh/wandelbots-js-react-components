e2e2c2d8a96e2527e26f7587dfd74c87fa3595a2
Bruno Bellmann <115152529+BrunoBellmannWandelbots@users.noreply.github.com>
2024-10-08 16:21:52 +0200

    fix: update abb models (#110)
    * fix/update-abb-models
    
    * fix/shadows-on-irb1200

--- src/components/robots/ABB_1200_07_7.tsx
+++ src/components/robots/ABB_1200_07_7.tsx
@@ -21,21 +21,17 @@ export function ABB_1200_07_7({
         <group name="visuals">
           <mesh
             name="visuals_1"
-            castShadow
-            receiveShadow
             geometry={nodes.visuals_1.geometry}
             material={materials.abb_metal_polished}
           />
           <mesh
             name="visuals_2"
-            castShadow
-            receiveShadow
             geometry={nodes.visuals_2.geometry}
             material={materials.abb_white}
           />
         </group>
         <animated.group name="ABB_IRB1200_J00">
-          <animated.group
+          <group
             name="ABB_IRB1200_J01"
             position={[0, 0.399, 0]}
             rotation={[-Math.PI / 2, -Math.PI / 2, 0]}
@@ -62,8 +58,6 @@ export function ABB_1200_07_7({
                     />
                     <mesh
                       name="visuals006"
-                      castShadow
-                      receiveShadow
                       geometry={nodes.visuals006.geometry}
                       material={materials.abb_metal_black}
                       position={[0.791, -0.351, 0]}
@@ -77,15 +71,11 @@ export function ABB_1200_07_7({
                   >
                     <mesh
                       name="visuals005_1"
-                      castShadow
-                      receiveShadow
                       geometry={nodes.visuals005_1.geometry}
                       material={materials.abb_metal_polished}
                     />
                     <mesh
                       name="visuals005_2"
-                      castShadow
-                      receiveShadow
                       geometry={nodes.visuals005_2.geometry}
                       material={materials.abb_white}
                     />
@@ -98,22 +88,16 @@ export function ABB_1200_07_7({
                 >
                   <mesh
                     name="visuals004_1"
-                    castShadow
-                    receiveShadow
                     geometry={nodes.visuals004_1.geometry}
                     material={materials.abb_metal_polished}
                   />
                   <mesh
                     name="visuals004_2"
-                    castShadow
-                    receiveShadow
                     geometry={nodes.visuals004_2.geometry}
                     material={materials.abb_white}
                   />
                   <mesh
                     name="visuals004_3"
-                    castShadow
-                    receiveShadow
                     geometry={nodes.visuals004_3.geometry}
                     material={materials.abb_red}
                   />
@@ -126,22 +110,16 @@ export function ABB_1200_07_7({
               >
                 <mesh
                   name="visuals003_1"
-                  castShadow
-                  receiveShadow
                   geometry={nodes.visuals003_1.geometry}
                   material={materials.abb_white}
                 />
                 <mesh
                   name="visuals003_2"
-                  castShadow
-                  receiveShadow
                   geometry={nodes.visuals003_2.geometry}
                   material={materials.abb_red}
                 />
                 <mesh
                   name="visuals003_3"
-                  castShadow
-                  receiveShadow
                   geometry={nodes.visuals003_3.geometry}
                   material={materials.abb_metal_polished}
                 />
@@ -154,32 +132,24 @@ export function ABB_1200_07_7({
             >
               <mesh
                 name="visuals002_1"
-                castShadow
-                receiveShadow
                 geometry={nodes.visuals002_1.geometry}
                 material={materials.abb_white}
               />
               <mesh
                 name="visuals002_2"
-                castShadow
-                receiveShadow
                 geometry={nodes.visuals002_2.geometry}
                 material={materials.abb_metal_polished}
               />
             </group>
-          </animated.group>
+          </group>
           <group name="visuals001">
             <mesh
               name="visuals001_1"
-              castShadow
-              receiveShadow
               geometry={nodes.visuals001_1.geometry}
               material={materials.abb_white}
             />
             <mesh
               name="visuals001_2"
-              castShadow
-              receiveShadow
               geometry={nodes.visuals001_2.geometry}
               material={materials.abb_metal_polished}
             />
