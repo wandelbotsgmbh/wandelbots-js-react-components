7bb29f019c40df080e4d39cad56772ac2f2de845
Evan Summers <evan.summers@wandelbots.com>
2024-10-25 17:02:00 +0200

    It kinda works!

---
+++ src/components/robots/GenericRobot.tsx
@@ -0,0 +1,57 @@
+import { animated } from "@react-spring/three"
+import { useGLTF } from "@react-three/drei"
+import type { Group, Mesh } from "three"
+import { type Object3D } from "three"
+import type { RobotModelProps } from "./types"
+
+GenericRobot.config = {
+  rotationOffsets: [0, -Math.PI / 2, 0, 0, 0, 0],
+}
+
+function isGroup(node: Object3D): node is Group {
+  return node.type === "Group"
+}
+
+function isMesh(node: Object3D): node is Mesh {
+  return node.type === "Mesh"
+}
+
+export function GenericRobot({
+  modelURL,
+  flangeRef,
+  ...props
+}: RobotModelProps) {
+  const gltf = useGLTF(modelURL)
+
+  const renderNode = (node: Object3D): React.ReactNode => {
+    if (isMesh(node)) {
+      return (
+        <mesh
+          name={node.name}
+          key={node.uuid}
+          geometry={node.geometry}
+          material={node.material}
+          position={node.position}
+          rotation={node.rotation}
+        />
+      )
+    } else {
+      return (
+        <animated.group
+          name={node.name}
+          key={node.uuid}
+          position={node.position}
+          rotation={node.rotation}
+        >
+          {node.children.map(renderNode)}
+        </animated.group>
+      )
+    }
+  }
+
+  return (
+    <group {...props} dispose={null}>
+      {renderNode(gltf.scene)}
+    </group>
+  )
+}
--- src/components/robots/RobotAnimator.tsx
+++ src/components/robots/RobotAnimator.tsx
@@ -1,9 +1,6 @@
-import { Globals, useSpring } from "@react-spring/three"
-import { useThree } from "@react-three/fiber"
 import type { MotionGroupStateResponse } from "@wandelbots/wandelbots-api-client"
-import React, { useRef } from "react"
+import React from "react"
 import type * as THREE from "three"
-import { useAutorun } from "../utils/hooks"
 import {
   getAllJointsByName,
   type RobotSceneJoint,
@@ -25,10 +22,7 @@ export default function RobotAnimator({
   robotConfig,
   children,
 }: RobotAnimatorProps) {
-  Globals.assign({ frameLoop: "always" })
-  const jointValues = useRef<number[]>([])
-  const jointObjects = useRef<THREE.Object3D[]>([])
-  const { invalidate } = useThree()
+  return <group>{children}</group>
 
   function setGroupRef(group: THREE.Group | null) {
     if (!group) return
@@ -47,9 +41,6 @@ export default function RobotAnimator({
     setSpring.start(Object.assign({}, jointValues.current) as any)
   }
 
-  const rotationSign = robotConfig?.rotationSign || [1, 1, 1, 1, 1, 1]
-  const rotationOffsets = robotConfig?.rotationOffsets || [0, 0, 0, 0, 0, 0]
-
   function setRotation() {
     const updatedJointValues = jointObjects.current.map((_, objectIndex) =>
       (axisValues as any)[objectIndex].get(),
@@ -65,29 +56,4 @@ export default function RobotAnimator({
       }
     }
   }
-
-  useAutorun(() => {
-    const newJointValues =
-      rapidlyChangingMotionState.state.joint_position.joints.filter(
-        (item) => item !== undefined,
-      )
-
-    requestAnimationFrame(() => updateJoints(newJointValues))
-  })
-
-  const [axisValues, setSpring] = useSpring(() => ({
-    ...Object.assign(
-      {},
-      rapidlyChangingMotionState.state.joint_position.joints,
-    ),
-    onChange: () => {
-      setRotation()
-      invalidate()
-    },
-    onResolve: () => {
-      setRotation()
-    },
-  }))
-
-  return <group ref={setGroupRef}>{children}</group>
 }
