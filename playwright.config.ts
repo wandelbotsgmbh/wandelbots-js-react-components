a7d77affe8584c3853f50ac4cb79f49a24698632
Vincent Gerber <vincent.gerber@wandelbots.com>
2024-10-29 10:01:38 +0100

    chore: Increased test timeout to 10min

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from "@playwright/test"
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5 * 60 * 1000,
+  timeout: 10 * 60 * 1000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
