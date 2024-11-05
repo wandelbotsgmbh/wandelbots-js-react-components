7c9f6bde4057d79deb118c8375cbfa18f4dd7a0d
Evan Summers <evan.summers@wandelbots.com>
2024-11-05 18:22:34 +0100

    Nicer default zoom on robot stories

--- stories/robots/SupportedRobotScene.tsx
+++ stories/robots/SupportedRobotScene.tsx
@@ -1,9 +1,22 @@
-import { useCallback } from "react"
+import { OrbitControls } from "@react-three/drei"
+import { Canvas, useThree } from "@react-three/fiber"
+import React, { useCallback, useState, type ReactNode } from "react"
 import type { Group } from "three"
-import { Mesh, MeshBasicMaterial, SphereGeometry } from "three"
-import { SupportedRobot } from "../../src"
+import {
+  Box3,
+  Mesh,
+  MeshBasicMaterial,
+  Sphere,
+  SphereGeometry,
+  Vector3,
+} from "three"
+import { PresetEnvironment, SupportedRobot } from "../../src"
 import { rapidlyChangingMotionState } from "./motionState"
 
+/**
+ * Renders a single robot model with orbit controls targeting it
+ * for storybook demonstration purposes
+ */
 export function SupportedRobotScene(
   props: React.ComponentProps<typeof SupportedRobot>,
 ) {
@@ -18,12 +31,61 @@ export function SupportedRobotScene(
   }, [])
 
   return (
-    <>
-      <SupportedRobot
-        {...props}
-        flangeRef={flangeRef}
-        rapidlyChangingMotionState={rapidlyChangingMotionState}
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        minHeight: "400px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Canvas shadows>
+        <PresetEnvironment />
+
+        <FitToCamera>
+          <SupportedRobot
+            {...props}
+            flangeRef={flangeRef}
+            rapidlyChangingMotionState={rapidlyChangingMotionState}
+          />
+        </FitToCamera>
+      </Canvas>
+    </div>
+  )
+}
+
+function FitToCamera({ children }: { children: ReactNode }) {
+  const { scene, camera } = useThree()
+  const [targetSphere, setTargetSphere] = useState(new Sphere())
+  const [lockZoom, setLockZoom] = useState(true)
+
+  const groupRef: React.RefCallback<Group> = useCallback(
+    (group) => {
+      if (!group) return
+
+      const bbox = new Box3().setFromObject(scene)
+      const center = bbox.getCenter(new Vector3())
+      const bsphere = bbox.getBoundingSphere(new Sphere(center))
+      setTargetSphere(bsphere)
+
+      setTimeout(() => {
+        setLockZoom(false)
+      }, 100)
+    },
+    [camera],
+  )
+
+  return (
+    <group ref={groupRef}>
+      {children}
+      <OrbitControls
+        camera={camera}
+        target={targetSphere.center}
+        minDistance={lockZoom ? targetSphere.radius * 3 : targetSphere.radius}
+        maxDistance={lockZoom ? targetSphere.radius * 3 : 3}
       />
-    </>
+    </group>
   )
 }
--- stories/robots/robotStoryConfig.tsx
+++ stories/robots/robotStoryConfig.tsx
@@ -2,9 +2,7 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { expect, fn, waitFor } from "@storybook/test"
 import type { DHParameter } from "@wandelbots/wandelbots-js"
-import { Vector3 } from "three"
 import { defaultGetModel, SupportedRobot } from "../../src"
-import { Setup } from "../../src/Setup"
 import { SupportedRobotScene } from "./SupportedRobotScene"
 
 type RobotJsonConfig = {
@@ -53,26 +51,6 @@ export const sharedStoryConfig = {
       }
     },
   },
-  decorators: [
-    (Story) => (
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          minHeight: "400px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Setup cameraPosition={new Vector3(0, 0, 3)}>
-          <group position={[0, -0.25, 0]}>
-            <Story />
-          </group>
-        </Setup>
-      </div>
-    ),
-  ],
 } satisfies Meta<typeof SupportedRobot>
 
 export function robotStory(
