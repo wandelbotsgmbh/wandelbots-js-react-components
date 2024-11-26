df5ba221e173c97e3db86f94beda9d1df7d822b5
Stefan Wagner <stefan.wagner@wandelbots.com>
2024-11-26 10:02:11 +0100

    feat: Replaced the ghost property with a setting to set transparent color  BREAKING CHANGE: The `isGhost` property has been removed in favor of `transparentColor` (#147)

--- src/components/robots/ghostStyle.ts
+++ src/components/robots/ghostStyle.ts
@@ -1,37 +1,38 @@
-import type { Group, Object3D } from "three"
-import { Material, Mesh, MeshStandardMaterial } from "three"
+import * as THREE from "three"
 
-export const applyGhostStyle = (robot: Group) => {
+export const applyGhostStyle = (robot: THREE.Group, color: string) => {
   if (robot.userData.isGhost) return
 
   robot.traverse((obj) => {
-    if (obj instanceof Mesh) {
-      if (obj.material instanceof Material) {
-        obj.material.colorWrite = true
+    if (obj instanceof THREE.Mesh) {
+      if (obj.material instanceof THREE.Material) {
+        obj.material.colorWrite = false
       }
 
       // Create a clone of the mesh
       const depth = obj.clone()
       const ghost = obj.clone()
 
-      depth.material = new MeshStandardMaterial({
+      depth.material = new THREE.MeshStandardMaterial({
         depthTest: true,
         depthWrite: true,
         colorWrite: false,
         polygonOffset: true,
-        polygonOffsetFactor: 1,
+        polygonOffsetFactor: -1,
+        side: THREE.DoubleSide,
       })
       depth.userData.isGhost = true
 
       // Set the material for the ghost mesh
-      ghost.material = new MeshStandardMaterial({
-        color: "#D91433",
+      ghost.material = new THREE.MeshStandardMaterial({
+        color: color,
         opacity: 0.3,
         depthTest: true,
         depthWrite: false,
         transparent: true,
         polygonOffset: true,
-        polygonOffsetFactor: -1,
+        polygonOffsetFactor: -2,
+        side: THREE.DoubleSide,
       })
       ghost.userData.isGhost = true
 
@@ -45,16 +46,16 @@ export const applyGhostStyle = (robot: Group) => {
   robot.userData.isGhost = true
 }
 
-export const removeGhostStyle = (robot: Group) => {
+export const removeGhostStyle = (robot: THREE.Group) => {
   if (!robot.userData.isGhost) return
 
-  const objectsToRemove: Object3D[] = []
+  const objectsToRemove: THREE.Object3D[] = []
 
   robot.traverse((obj) => {
-    if (obj instanceof Mesh) {
+    if (obj instanceof THREE.Mesh) {
       if (obj.userData?.isGhost) {
         objectsToRemove.push(obj)
-      } else if (obj.material instanceof Material) {
+      } else if (obj.material instanceof THREE.Material) {
         obj.material.colorWrite = true
       }
     }
@@ -66,5 +67,5 @@ export const removeGhostStyle = (robot: Group) => {
     }
   })
 
-  robot.userData.isGhost = true
+  robot.userData.isGhost = false
 }
