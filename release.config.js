812abb6eb389e461ce122afd0295897506a6da17
Stefan Wagner <stefan.wagner@wandelbots.com>
2025-02-24 12:43:08 +0100

    fix: Update release workflow to create a zip archive of no-draco models

--- release.config.js
+++ release.config.js
@@ -1,17 +1,21 @@
 export default {
-  branches: [
-    "main",
-  ],
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    
+
     [
       "@semantic-release/github",
       // Workaround for rate limit issue on github
       // https://github.com/semantic-release/semantic-release/issues/2204#issuecomment-2154938064
       {
+        assets: [
+          {
+            path: "no-draco-models.zip",
+            label: "Uncompressed 3D Models (No Draco)",
+          },
+        ],
         successComment: false,
         failTitle: false,
       },
