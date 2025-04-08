ddb4298d752f67ec5564c9ced0f8a18964e4691f
Evan Summers <evan.summers@wandelbots.com>
2025-04-08 11:55:46 +0200

    Use non-default export for i18n instance

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,10 +1,10 @@
-import type { i18n } from "i18next"
+import type { i18n as I18NInstance } from "i18next"
 import i18next from "i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
 import deJSON from "./locales/de/translations.json"
 import enJSON from "./locales/en/translations.json"
 
-const i18n: i18n = i18next.createInstance()
+const i18n: I18NInstance = i18next.createInstance()
 
 i18n.use(LanguageDetector).init({
   supportedLngs: ["en", "de"],
@@ -23,4 +23,4 @@ i18n.use(LanguageDetector).init({
   },
 })
 
-export default i18n
+export { i18n }
