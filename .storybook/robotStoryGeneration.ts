d609099ea08a11e7ee02b992e62f494748923b35
Bruno Bellmann <115152529+BrunoBellmannWandelbots@users.noreply.github.com>
2025-07-30 10:54:55 +0200

    feat: add kuka dkp500-2 (#369)
    [Storybook
    Link](https://wandelbotsgmbh.github.io/wandelbots-js-react-components/overview.html)

--- .storybook/robotStoryGeneration.ts
+++ .storybook/robotStoryGeneration.ts
@@ -15,6 +15,7 @@ const HIDDEN_ROBOTS = [
   "KUKA_KR270_R2700",
   "FANUC_M20iD35",
   "YASKAWA_TURN2",
+  "KUKA_DKP500_2"
 ]
 
 /** Generates lines of CSF code defining stories for each robot model .glb file */
