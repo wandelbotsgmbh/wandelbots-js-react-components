ad1881814f966c96b1e6a70ca1afbe6e6f027c26
3m5. Marcin Szalomski <marcin.szalomski@partner.wandelbots.com>
2026-04-08 13:18:05 +0200

    RB-3348: REMOVE unused import

--- src/components/3d-viewport/SafetyZonesRenderer.tsx
+++ src/components/3d-viewport/SafetyZonesRenderer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react"
+import { useMemo } from "react"
 import { type ThreeElements } from "@react-three/fiber"
 import * as THREE from "three"
 import { ConvexGeometry, RoundedBoxGeometry } from "three-stdlib"
