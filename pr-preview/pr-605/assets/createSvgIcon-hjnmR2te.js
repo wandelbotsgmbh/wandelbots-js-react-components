1203322136cac3cc9da9da2fc9c1aa2c443db3ed
MarcinSzalomski <MarcinSzalomski@users.noreply.github.com>
2026-05-20 12:25:15 +0000

    Deploy preview for PR 605 🛫

---
+++ pr-preview/pr-605/assets/createSvgIcon-hjnmR2te.js
@@ -0,0 +1,1 @@
+import{r as o,j as n}from"./iframe-CxmfdHuz.js";import{S as t}from"./SvgIcon-C3bJ3kMJ.js";function p(e,i){function r(a,m){return n.jsx(t,{"data-testid":void 0,ref:m,...a,children:e})}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{p as c};
--- pr-preview/pr-605/assets/styled-C-S2gy1s.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./Typography-CbqdGqIA.js";const s=t();export{s};
