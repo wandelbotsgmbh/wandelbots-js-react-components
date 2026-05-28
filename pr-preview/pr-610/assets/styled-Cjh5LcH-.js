d0eadc886921e63be887467610d362cbd6ead751
Strieglitz <Strieglitz@users.noreply.github.com>
2026-05-28 14:47:02 +0000

    Remove preview for PR 610 🛬

--- pr-preview/pr-610/assets/styled-Cjh5LcH-.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./Typography-Bko41uo5.js";const s=t();export{s};
--- pr-preview/pr-610/assets/useThemeProps-BaaiqUtG.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./getThemeProps-iz1S-R3y.js";import{u}from"./iframe-BiSlx2o3.js";function i({props:o,name:m,defaultTheme:s,themeId:r}){let e=u(s);return r&&(e=e[r]||e),t({theme:e,name:m,props:o})}export{i as u};
