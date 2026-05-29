a209b774ac2d5172e3643c96fcbdb0302bd592c6
Strieglitz <Strieglitz@users.noreply.github.com>
2026-05-29 09:41:44 +0000

    Remove preview for PR 613 🛬

--- pr-preview/pr-613/assets/styled-YxX8NoS3.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./Typography-SbE6EZMf.js";const s=t();export{s};
--- pr-preview/pr-613/assets/useThemeProps-cJFIFyYJ.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./getThemeProps-CvU0DA3I.js";import{u}from"./iframe-DzkduSN5.js";function i({props:o,name:m,defaultTheme:s,themeId:r}){let e=u(s);return r&&(e=e[r]||e),t({theme:e,name:m,props:o})}export{i as u};
